281ea0d99799010b6f02d64f6f32ae6933b97284
Divya Khaturiya <khaturiyadiv@gmail.com>
2026-08-10 16:27:13 +0000

    Update Numbers.js

--- Numbers.js
+++ Numbers.js
@@ -6,3 +6,6 @@ console.log(age.toString().length)
 console.log(age.toFixed(1))
 const guess=56555.5658
 console.log(guess.toPrecision(3))
+const num2=256013
+console.log(num2.toLocaleString('en-IN'))
+
