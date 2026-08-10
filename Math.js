2bd0cc7a0ff80ab474a0b3bc620d846a3e65f352
Divya Khaturiya <khaturiyadiv@gmail.com>
2026-08-10 16:27:54 +0000

    Update Numbers and Math files

---
+++ Math.js
@@ -0,0 +1,16 @@
+console.log(Math)
+//math is object 
+console.log(Math.abs(-6))
+//abs remove negative sign
+console.log(Math.round(56.325))
+//round turn number into nearest integer
+console.log(Math.ceil(56.32))
+//ceil turns number into greatest integer
+console.log(Math.floor(5.6))
+// floor turns number into smallest integer
+console.log(Math.min(56,3,99,65))
+console.log(Math.max(56,55,39,99))
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
