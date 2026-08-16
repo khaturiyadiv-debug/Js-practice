feead3381690b02273622c30cc971f1bed9389cb
Divya Khaturiya <khaturiyadiv@gmail.com>
2026-08-16 14:21:33 +0000

    Updating code

--- logical.js
+++ logical.js
@@ -11,3 +11,22 @@ else if( subscription="Free"&& age>18 && email.includes(".com")){
 else{
     console.log("Sorry we can't fetch your data")
 }
+
+let year=2010
+switch(true){
+    case(year>1965&&year<=1980):
+    console.log("Generation X")
+    break;
+    case(year>1980&& year<=1996):
+    console.log("Millenials")
+    break;
+    case(year>1996&& year<=2012):
+    console.log("GenZ")
+    break;
+    case(year>2012&&year<=2024):
+    console.log("Gen-Alpha")
+    break;
+    
+        
+    
+}
