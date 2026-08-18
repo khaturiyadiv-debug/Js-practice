29f02988a5629d4cbce661d309ac8f54c4ff55dd
Divya Khaturiya <khaturiyadiv@gmail.com>
2026-08-18 13:18:40 +0000

    Updating loop.js

--- loop.js
+++ loop.js
@@ -6,3 +6,18 @@ let ar1=['batman','superman','flash']
 for(let j=0;j<=(ar1.length);j++){
     console.log(ar1[j])
 }
+for (let i = 0; i <= 10; i++) {
+
+    if (i === 9) {
+        break;
+    }
+
+    for (let k = 0; k <= 10; k++) {
+
+        if (i === 6) {
+            continue;
+        }
+
+        console.log(i, '*', k, '=', i * k);
+    }
+}
