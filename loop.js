94bd1c5cf00af8a84e82e57d0dc877e3388b69d1
Divya Khaturiya <khaturiyadiv@gmail.com>
2026-08-18 14:34:00 +0000

    performing changes

--- loop.js
+++ loop.js
@@ -21,3 +21,20 @@ for (let i = 0; i <= 10; i++) {
         console.log(i, '*', k, '=', i * k);
     }
 }
+
+flower=['marigold','dandelion','rose','tulip']
+let i=0
+while(i<flower.length){
+    console.log(flower[i])
+    i++
+}
+
+// diff between while and do while is
+//in while firstly condition and checked then loop performs action
+//in do while firstly code runs then coding is checked
+
+let f=0
+do{
+    console.log(`welcome to js practice repo`)
+    f++
+}while(f<5);
