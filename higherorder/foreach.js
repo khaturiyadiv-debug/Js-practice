7c07a1524f74ef4af65f4cf4bae44cc3e462eb57
Divya Khaturiya <khaturiyadiv@gmail.com>
2026-08-18 16:01:53 +0000

    loops

---
+++ higherorder/foreach.js
@@ -0,0 +1,21 @@
+let dishes = [
+    "Paneer Tikka","Masala Dosa","Chole Bhature","Rajma Chawal","Palak Paneer","Aloo Paratha",
+    "Pav Bhaji"]
+dishes.forEach(function(order,index,array){
+    console.log(order,index,array)
+})
+
+let myar=[
+    {ai:'Chatgpt'},
+    {ai:"Gemini"},
+    {ai:'Copilot'},
+    {ai:"Perplexity AI"}
+]
+
+myar.forEach((obj)=>{
+    console.log(obj.ai)
+})
+  
+    
+    
+
